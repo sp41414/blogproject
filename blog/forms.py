47c249399559521624ebaa34c8c5b8c4f452b334
sp4141 <134581084+sp41414@users.noreply.github.com>
2025-02-15 19:30:48 +0400

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -13,4 +13,4 @@ class SignupForm(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'status']
